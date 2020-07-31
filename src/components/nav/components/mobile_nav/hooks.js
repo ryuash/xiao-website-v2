5a6dacf065694d0a04b9522f8dc68d57e0c28995
Wingman <ryuash123@gmail.com>
2020-07-31 20:53:07 +0800

    fix: mobile bars

--- src/components/nav/components/mobile_nav/hooks.js
+++ src/components/nav/components/mobile_nav/hooks.js
@@ -1,7 +1,16 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { sizes } from '@styles'
+import useWindowSize from '@utils/screenSize'
 
 export const useMobileNavHook = () => {
   const [isOpen, toggleOpen] = useState(false)
+  const { width } = useWindowSize()
+
+  useEffect(() => {
+    if (width >= sizes.tablet) {
+      toggleOpen(false)
+    }
+  }, [width])
 
   const toggle = () => {
     toggleOpen(!isOpen)
